a89b800f663e91a72c9e52bf7ec6542f7bea03f6
giorgi200706 <giorgich31@gmail.com>
2023-12-10 23:58:13 +0400

    121

---
+++ Array-String/121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let maxProfit = 0;
+  let minPrice = Number.MAX_VALUE;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+
+  return maxProfit;
+};
